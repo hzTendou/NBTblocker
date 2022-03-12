4eb79a8211f7f4869daecbcbd6509c213ce03725
xTendou <80038372+xTendou@users.noreply.github.com>
2022-03-12 22:29:56 +0400

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,33 +1,58 @@
-import { world, MinecraftBlockTypes, BlockLocation } from 'mojang-minecraft'
-
-
+ import { world, MinecraftBlockTypes, BlockLocation } from 'mojang-minecraft'
 
 world.events.blockPlace.subscribe(ev => {
+
   const { block, player, dimension } = ev
+
   const { x, y, z } = block.location
-  
+
   let ilegalBlocks = [
+
     'minecraft:movingBlock',
+
     'minecraft:movingblock',
+
     'minecraft:glowingobsidian',
-    'minecraft:glowingObsidian',
-    'minecraft:reserved6',
-    'minecraft:reserved2',
-    'minecraft:reserved3',
-    'minecraft:info_update',
-    'minecraft:info_update2'
+
+    'minecraft:glowingObsidian'
+
     ]
-    
+
     if(ilegalBlocks.includes(block.id)) {
-      dimension.runCommand(`tellraw @a {"rawtext":[{"text":"§cDikkat! §7${player.nameTag} §cisimli oyucu: §e${block?.id} bloğunu §c${x} ${y} ${z} §akordinatına koydu!"}]}`)
+
+      dimension.runCommand(`tellraw @a {"rawtext":[{"text":"§cDikkat! §7${player.nameTag} §cisimli oyucu: §e${block?.id} bloğunu §c${x} ${y} ${z} §akordinatına koydu! (AntiNb)"}]}`)
+
       player.runCommand(`tag @s add banned`)
+
       dimension.getBlock(new BlockLocation(x, y, z)).setType(MinecraftBlockTypes.air)
+
     }
+
+})
+
+worlds.events.tick.subscribe(data => {
+
+  for(const player of world.getPlayers()){
+
+    let rightInventoryComp = player.getComponent("minecraft:inventory")
+
+    let rightChestContainer = rightInventoryComp.container
+
+    let item = rightChestContainer.getItem(player.selectedSlot)
+
+    player.runCommand(`tag @s add banned`)
+
+  }
+
 })
 
 world.events.tick.subscribe(data => {
+
   world.getDimension("overworld").runCommand(`tp @a[tag=banned] 9999999 99999 9999999`)
+
   world.getDimension("overworld").runCommand(`tellraw @a[tag=banned] {"rawtext":[{"text":"§4BU SERVERDEN BANLANDIN AGLA EZİK"}]}`)
-  world.getDimension("overworld").runCommand(`title @a[tag=banned] title §4§lBU SERVERDEN BANLANDIN`)
-  world.getDimension("overworld").runCommand(`kick @a[tag=banned] §4Banlandın sebeb; hile açmak`)
+
+  world.getDimension("overworld").runCommand(`title @a[tag=banned] title §4§lSERVERDEN BANLANDIN`)
+
 })
+
